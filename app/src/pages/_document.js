b156321765c131cb2613ae1cd18bf7d5f0f90bdb
mozlercelik <celikmehmetozler@gmail.com>
2024-02-21 12:50:32 +0300

    Enable google translate on website

--- app/src/pages/_document.js
+++ app/src/pages/_document.js
@@ -7,7 +7,7 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 class CustomDocument extends Document {
     render() {
         return (
-            <Html lang="en">
+            <Html lang="en-US">
                 <Head>
                     <link
                         rel="preconnect"
@@ -19,6 +19,8 @@ class CustomDocument extends Document {
                         href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap"
                     />
                     <link rel="shortcut icon" href="/images/favicon.png" />
+
+                    <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
                 </Head>
                 <body>
                     <Main />

d2886898424e361537341bdc507b590ff573a2bc
mozlercelik <celikmehmetozler@gmail.com>
2024-02-21 12:57:40 +0300

    update card's shadow

--- app/src/theme/overrides/card.js
+++ app/src/theme/overrides/card.js
@@ -7,7 +7,9 @@ const card = theme => {
         root: {
           borderRadius: "5px",
           backgroundColor: hexToRGBA(theme.palette.background.paper, 0.2),
-          boxShadow: "0px 1px 15px rgba(0, 0, 0, 0.25)",
+          boxShadow: `0px 1px 5px ${theme.palette.secondary.main}`,
+          // boxShadow: 'none',
+          // border: `1px solid ${theme.palette.secondary.main}`,
           // boxShadow: "none",
           // zIndex: 100,
           // "& .MuiCardContent-root": {
